6c976b990099cc9b60eff1fd06ec127fa5878b7d
User <Willspri@gmail.com>
2024-03-28 10:38:55 +1300

    completed

--- FileObfuscate/FileObfuscate.py
+++ FileObfuscate/FileObfuscate.py
@@ -14,22 +14,22 @@ def toHex(inputFile, outputFile):
         print("File not found")
         
 #inputFile = input("enter path to file: ")
-inputFile = "sandwich.png"
+inputFile = "filename"
 outputFile = "hex.txt"
 toHex(inputFile, outputFile)
 
 #take hex.txt and perform vigenere, save as encrypted.txt
 def vigenereEncrypt(plaintext, key):
     encrypted = ''
-    key_index = 0
+    keyIndex = 0
     for char in plaintext:
         if char.isalpha():
-            shift = ord(key[key_index % len(key)].lower()) - ord('a')
+            shift = ord(key[keyIndex % len(key)].lower()) - ord('a')
             if char.islower():
                 encrypted += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
             else:
                 encrypted += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
-            key_index += 1
+            keyIndex += 1
         else:
             encrypted += char
     return encrypted
@@ -39,10 +39,10 @@ def encryptFile(inputFile2, outputFile2, key):
         with open(inputFile2, 'r') as f:
             plaintext = f.read()
             
-        encrypted_text = vigenereEncrypt(plaintext, key)
+        encryptedText = vigenereEncrypt(plaintext, key)
         
         with open(outputFile2, 'w') as f:
-            f.write(encrypted_text)
+            f.write(encryptedText)
         
         print("Encryption complete. Saved to", outputFile2)
     except FileNotFoundError:
@@ -52,7 +52,70 @@ inputFile2 = "hex.txt"
 outputFile2 = "encrypted.txt"
 key = input("Enter encryption key: ")
 
-#toHex(inputFile, outputFile)
+
 encryptFile(inputFile2, outputFile2, key)
+
 #take encrypted.txt and decode to decoded.txt
+def decryptFile(inputFile3, outputFile, key):
+    try:
+        with open(inputFile3, 'r') as f:
+            encryptedText = f.read()
+
+        decryptedText = vigenereDecrypt(encryptedText, key)
+
+        with open(outputFile, 'w') as f:
+            f.write(decryptedText)
+
+        print("Decryption complete. Saved to", outputFile)
+    except FileNotFoundError:
+        print("File not found")
+
+def vigenereDecrypt(ciphertext, key):
+    decryptedText = ""
+    keyLength = len(key)
+    keyIndex = 0
+    for char in ciphertext:
+        if char.isalpha():
+            shift = ord(key[keyIndex % keyLength].lower()) - ord('a')
+            if char.isupper():
+                decryptedText += chr(((ord(char) - ord('A') - shift) % 26) + ord('A'))
+            else:
+                decryptedText += chr(((ord(char) - ord('a') - shift) % 26) + ord('a'))
+            keyIndex += 1
+        else:
+            decryptedText += char
+    return decryptedText
+
+
+inputFile3 = "encrypted.txt"
+outputFile = "decrypted.txt"
+key = input("Enter decryption key: ")
+decryptFile(inputFile3, outputFile, key)
+
+
 #take decoded.txt and recompile as recompiled.'filetype'
+
+def hexRecompile(inputFile4, outputFile3):
+    try:
+        with open(inputFile4, 'r') as f:
+            hex_data = f.read().strip()
+
+        if len(hex_data) % 2 != 0:
+            raise ValueError("Invalid hex data length")
+
+        originalData = bytes.fromhex(hex_data)
+
+        with open(outputFile3, 'wb') as f:
+            f.write(originalData)
+
+        print("Recompiled file saved to", outputFile3)
+
+    except FileNotFoundError:
+        print("File not found.")
+    except ValueError as e:
+        print("Error:", e)
+        
+inputFile4 = "decrypted.txt"
+outputFile3 = "recompiled" #user must add correct extension
+
+hexRecompile(inputFile4, outputFile3)
