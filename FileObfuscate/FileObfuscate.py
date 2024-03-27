ace5cf49c919e9522fac48d6143114fcc8ea4ecb
User <Willspri@gmail.com>
2024-03-28 09:24:37 +1300

    Add project files.

---
+++ FileObfuscate/FileObfuscate.py
@@ -0,0 +1,5 @@
+
+#take file and save as hex.txt
+#take hex.txt and perform vigenere, save as encrypted.txt
+#take encrypted.txt and decode to decoded.txt
+#take decoded.txt and recompile as recompiled.'filetype'
